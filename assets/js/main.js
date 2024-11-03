edcd016814d0a022ba1203ae6fd340b2feea6e54
Jared Watrous <jwatrous2002@gmail.com>
2024-11-03 03:11:59 -0800

    Fix more image viewer bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 
 var viewImage = function(img) {
     $('#imgviewer').attr('src', $(img).attr('src'));
-    $('#imgviewercontainer').fadeIn(200);
+    $('#imgviewerfade').fadeIn(200);
 }
 var stopViewingImage = function() {
-    $('#imgviewercontainer').fadeOut(200);
+    $('#imgviewerfade').fadeOut(200);
 }
 
 var startup = function() {
-    $('#imgviewercontainer').click(stopViewingImage);
+    $('#imgviewerfade').click(stopViewingImage);
     $('img').each(function() {
         $(this).click(() => { viewImage(this); })
     });
