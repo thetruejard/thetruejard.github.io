058727e847f50a6834dd1759400f3098e09f0a92
Jared Watrous <45702341+thetruejard@users.noreply.github.com>
2024-11-03 20:07:02 -0800

    Gallery controls

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,22 @@
 
+var galleryEntry = null;
+var galleryNext = function() {
+    if (galleryEntry == null || !$(galleryEntry).next()) return;
+    viewImage($(galleryEntry).next());
+}
+var galleryPrev = function() {
+    if (galleryEntry == null || !$(galleryEntry).prev()) return;
+    viewImage($(galleryEntry).prev());
+}
+
 var viewImage = function(img) {
+    galleryEntry = img;
     $('#imgviewer').attr('src', $(img).attr('src'));
-    $('#imgviewerfade').fadeIn(200);
+    if ($('#imgviewerfade').css('display') == 'none')
+        $('#imgviewerfade').fadeIn(200);
 }
 var stopViewingImage = function() {
+    galleryEntry = null;
     $('#imgviewerfade').fadeOut(200);
 }
 
@@ -14,5 +27,15 @@ var startup = function() {
             $(this).click(() => { viewImage(this); })
         }
     });
+    document.onkeydown = function(e) {
+        switch(e.code) {
+            case 'ArrowLeft':
+                galleryPrev();
+                break;
+            case 'ArrowRight':
+                galleryNext();
+                break;
+        }
+    };
 }
 $(document).ready(startup);
